295c445364b974912f2884ef3b1e14f126a47f32
qa521357 <qa521357@LR00901.southeast.edu>
2017-09-13 10:59:39 -0500

    changes to cartcontroller

--- src/java/controller/CartController.java
+++ src/java/controller/CartController.java
@@ -30,7 +30,7 @@ public class CartController extends HttpServlet {
 
         //Product oProduct = HuskerDA.getSpecificProduct("1");
         String sAction = request.getParameter("action");
-        if(sAction.equals("add")){
+        if(sAction.equals("cart")){
             String sProductID = request.getParameter("productID");
         String sQuantity = request.getParameter("quantity");
         int quantity = 0;
@@ -45,12 +45,25 @@ public class CartController extends HttpServlet {
         LineItem lineItem = new LineItem();
         lineItem.setProduct(oProduct);
             lineItem.setQuantity(quantity);
+        if(quantity == 0){
+        //remove item
+        oCart.removeItem(lineItem);
+        }
+        else if(quantity >0){
+            //add item logic
+        
+        
         oCart.addItem(lineItem);
+        }
+
              
        
          oSession.setAttribute("oCart", oCart);
          
           url = "/products.jsp";  
+        }
+        else if(sAction.equals("remove")){
+            
         }
         
             
